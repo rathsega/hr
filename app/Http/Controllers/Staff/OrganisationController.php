e0884b73240bb3fe77da0a5c81e015c038c9d4c2
sekhar <sekhar5b0@gmail.com>
2024-03-08 16:52:56 +0530

    Organisation Structure Issue

--- app/Http/Controllers/Staff/OrganisationController.php
+++ app/Http/Controllers/Staff/OrganisationController.php
@@ -30,9 +30,9 @@ class OrganisationController extends Controller
         ->get();*/
 
         $all_users_structure = DB::select("WITH RECURSIVE ReportingStructure AS (
-            SELECT id, name, photo, designation, 0 AS manager, 0 AS level
+            SELECT id, name, photo, designation, 0 AS manager, 1 AS level
             FROM users
-            WHERE manager IS NULL  -- Assuming root employees have NULL in the manager column
+            WHERE manager = 0   -- Assuming root employees have NULL in the manager column
           
             UNION ALL
           
@@ -44,7 +44,17 @@ class OrganisationController extends Controller
           SELECT id, manager as parent, case when photo is null then CONCAT('<img src=/hr/public/uploads/user-image/placeholder/placeholder.png></br> ', name,'<p class=designation>(',designation,')</p>') else CONCAT('<img src=/hr/public/uploads/user-image/', photo,'></br> ', name,'<p class=designation>(',designation,')</p>') end as name 
           FROM ReportingStructure");
         
-        $page_data['all_users_structure'] = (array)$all_users_structure;
+        $data = (array)$all_users_structure;
+        //var_dump($data);exit;
+        //$page_data['all_users_structure'][0] = array('id'=> null, 'name'=> '', 'parent'=> 0);
+        foreach($data as $key => $val){
+            if($val->parent == 0){
+                $data[$key]->parent = 9999999;
+            }
+        }
+        array_unshift($data, array('id'=> 9999999, 'name'=> '<img class="first-node" src=/hr/public/assets/images/zettamine-logo.png><p class=designation>Zettamine</p>', 'parent'=> 0));
+
+        $page_data['all_users_structure'] = $data;
 
         $page_data['single_structure'] = DB::select("WITH RECURSIVE EmployeeStructure AS ( SELECT id, name, manager, photo FROM users WHERE id = ". auth()->user()->id ." UNION ALL SELECT u.id, u.name, u.manager, u.photo FROM users u JOIN EmployeeStructure es ON u.id = es.manager ) SELECT id, case when photo is null then CONCAT('<img src=/hr/public/uploads/user-image/placeholder/placeholder.png></br> ', name) else CONCAT('<img src=/hr/public/uploads/user-image/', photo,'></br> ', name) end as name ,case when manager is NULL THEN 0 ELSE manager END as parent FROM EmployeeStructure");
         
