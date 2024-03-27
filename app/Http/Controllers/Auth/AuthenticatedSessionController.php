0d7e3698f2de40daaf30a3ae50ee9f5f5025a224
sekhar <sekhar5b0@gmail.com>
2024-03-27 22:40:02 +0530

    Login Page Slider

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -17,7 +18,46 @@ class AuthenticatedSessionController extends Controller
      */
     public function create()
     {
-        return view('auth.login');
+        $current_date = date('Y-m-d');
+        $birthday_users = DB::select("SELECT name, photo FROM users WHERE DATE_FORMAT(birthday, '%m-%d') = DATE_FORMAT('$current_date', '%m-%d') and status='active'");
+        $today_quote = DB::select("SELECT q.*, u.id as user_id, u.name, u.designation, u.photo from quotes as q inner join users as u on u.id = q.user_id WHERE DATE(q.from_date) <= CURDATE() and DATE(q.to_date) >= CURDATE()");
+        $employee_of_the_month = DB::select("select e.*, e.id as id, u.id as user_id, u.name, u.photo, u.designation, u.emp_id, u.email from employee_of_the_month as e inner join users as u on u.id = e.user_id WHERE e.month = MONTH(CURRENT_DATE() - INTERVAL 1 MONTH)
+        AND e.year = YEAR(CURRENT_DATE() - INTERVAL 1 MONTH)");
+
+        $data = [];
+
+        if($birthday_users){
+            foreach($birthday_users as $key => $birthday_user){
+                $slider = [];
+                $slider['type'] = "birthday";
+                $slider['name'] = $birthday_user->name;
+                $slider['photo'] = $birthday_user->photo;
+                $data[] = $slider;
+            }
+        }
+
+        if($today_quote){
+            foreach($today_quote as $key => $quote){
+                $slider = [];
+                $slider['type'] = "quote";
+                $slider['name'] = $quote->name;
+                $slider['photo'] = $quote->photo;
+                $slider['quote'] = $quote->quote;
+                $slider['designation'] = $quote->designation;
+                $data[] = $slider;
+            }
+        }
+        if($employee_of_the_month){
+            foreach($employee_of_the_month as $key => $employee){
+                $slider = [];
+                $slider['type'] = "eotm";
+                $slider['name'] = $employee->name;
+                $slider['photo'] = $employee->photo;
+                $slider['designation'] = $employee->designation;
+                $data[] = $slider;
+            }
+        }
+        return view('auth.login', array('slides'=>$data));
     }
 
     /**
